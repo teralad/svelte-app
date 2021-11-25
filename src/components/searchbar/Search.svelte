cf7d207953883e97dfbfb1abed690dab8a971c15
Varun Kumaar <teralad@gmail.com>
2021-11-25 18:21:39 +0530

    adding static and search bar

---
+++ src/components/searchbar/Search.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let query;
+</script>
+
+<style>
+  .search-input {
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    padding: 10px 20px;
+    font-size: 20px;
+    margin-bottom: 50px;
+  }
+</style>
+
+<div class="search">
+  <form class="search-form" >
+    <input bind:value={query} class="search-input" type="search"
+    placeholder="Search decathlon's library of over 1 million products" />
+  </form>
+</div>
---
+++ src/lib/header/IconButton.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Icon from './Icon.svelte'
+  export let title
+</script>
+
+
+<div class="iconbutton" {title}>
+  <Icon {title}/>
+  <p>
+    {title.replaceAll('-', ' ').toUpperCase()}
+  </p>  
+</div>
+
+<style>
+  .iconbutton {
+    justify-content: center;
+    margin: 3px;
+    position: relative;
+  }
+
+  .iconbutton p {
+    align-items: center;
+    font-size: xx-small;
+    font-weight: bold;
+    align-content: center;
+  }
+</style>
